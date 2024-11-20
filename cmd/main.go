a7dcf38afafbe73c8f044cda78ba158db087ad21
Matteo Vollero <m.vollero96@gmail.com>
2024-11-20 11:04:06 +0100

    Boilerplate for config file opening + Config struct creation

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"companies_handling/internal"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+)
+
 func main() {
 
+	// Open the config.yaml file for configurations
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	f := strings.Split(wd, "/")
+	f = f[:len(f)-1]             // Removing the last dir in the past
+	f = append(f, "config.yaml") // Adding the file name as leaf of the path
+	cp := strings.Join(f, "/")   // Join to have the proper position
+	cf, err := os.Open(cp)
+	if err != nil {
+		panic(err)
+	}
+	defer cf.Close()
+
+	cb, err := io.ReadAll(cf)
+	if err != nil {
+		panic(err)
+	}
+	// Decode yaml file
+	var config internal.Config
+	if err := yaml.Unmarshal(cb, &config); err != nil {
+		panic(err)
+	}
+	fmt.Println(config)
 }
--- internal/models.go
+++ internal/models.go
@@ -1,3 +1,12 @@
 package internal
 
 // @remind : Develop models here
+type Config struct {
+	Database struct {
+		Host     string `yaml:"host"`
+		Port     int    `yaml:"port"`
+		User     string `yaml:"user"`
+		Password string `yaml:"password"`
+		Name     string `yaml:"name"`
+	}
+}
