e833fef3d41dd9b198a3dbe22e39652cb3220ff2
Matteo Vollero <m.vollero96@gmail.com>
2024-11-20 17:49:08 +0100

    Created void endpoints + void middleware, only structure

---
+++ routes/routes.go
@@ -0,0 +1,17 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+func SetUpRoutes(router *gin.Engine) {
+	router.POST("/user", nil)
+	router.GET("/user", nil)
+	userGroup := router.Group("/user")
+	// @todo : Here you will check the user and restrict the access via middlware
+	userGroup.Use(nil)
+	{
+		router.POST("/companies", nil)
+		router.PATCH("/companies/:id", nil)
+		router.GET("/companies/:id", nil)
+		router.DELETE("/companies/:id", nil)
+	}
+}
