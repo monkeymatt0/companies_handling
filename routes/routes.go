9690ce4b4129a1dbb774da6dc5700da77661072a
Matteo Vollero <m.vollero96@gmail.com>
2024-11-21 13:25:24 +0100

    Company CRUD handlers

--- routes/routes.go
+++ routes/routes.go
@@ -7,21 +7,29 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func SetUpRoutes(router *gin.Engine, userHandler *handlers.UserHandler) {
+func SetUpRoutes(
+	router *gin.Engine,
+	userHandler *handlers.UserHandler,
+	companyHandler *handlers.CompanyHandler,
+) {
 	router.POST("/users", userHandler.CreateUser)
 	router.POST("/login", userHandler.LoginUser)
-	router.GET("/companies/:id", nil)
+	router.GET("/companies/:uuid", companyHandler.GetCompany)
 
 	userGroup := router.Group("/users")
-	// @todo : Here you will check the user and restrict the access via middlware
 	userGroup.Use(middlewares.JwtCheck)
 	{
 		userGroup.GET("/:id", userHandler.GetUser)
 		userGroup.DELETE("/:id", userHandler.DeleteUser)
 		userGroup.DELETE("/:id/hard", userHandler.DeleteUserHard)
+	}
 
-		userGroup.POST("/companies", nil)
-		userGroup.PATCH("/companies/:id", nil)
-		userGroup.DELETE("/companies/:id", nil)
+	authGroup := router.Group("/")
+	authGroup.Use(middlewares.JwtCheck)
+	{
+		authGroup.POST("/companies", companyHandler.CreateCompany)
+		authGroup.PATCH("/companies/:uuid", companyHandler.EditCompany)
+		authGroup.DELETE("/companies/:uuid", companyHandler.DeleteCompany)
+		authGroup.DELETE("/companies/:uuid/hard", companyHandler.DeleteCompanyHard)
 	}
 }
