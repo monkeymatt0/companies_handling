d4bf343c525c34add4ef3d3dfd172effbfa6b051
Matteo Vollero <m.vollero96@gmail.com>
2024-11-21 01:09:46 +0100

    Updated routes

--- routes/routes.go
+++ routes/routes.go
@@ -8,20 +8,20 @@ import (
 )
 
 func SetUpRoutes(router *gin.Engine, userHandler *handlers.UserHandler) {
-	router.POST("/user", userHandler.CreateUser)
+	router.POST("/users", userHandler.CreateUser)
 	router.POST("/login", userHandler.LoginUser)
 	router.GET("/companies/:id", nil)
 
-	userGroup := router.Group("/user")
+	userGroup := router.Group("/users")
 	// @todo : Here you will check the user and restrict the access via middlware
 	userGroup.Use(middlewares.JwtCheck)
 	{
-		router.GET("/user/:id", userHandler.GetUser)
+		userGroup.GET("/:id", userHandler.GetUser)
 		userGroup.DELETE("/:id", userHandler.DeleteUser)
 		userGroup.DELETE("/:id/hard", userHandler.DeleteUserHard)
 
-		// userGroup.POST("/companies", nil)
-		// userGroup.PATCH("/companies/:id", nil)
-		// userGroup.DELETE("/companies/:id", nil)
+		userGroup.POST("/companies", nil)
+		userGroup.PATCH("/companies/:id", nil)
+		userGroup.DELETE("/companies/:id", nil)
 	}
 }
